8cf695844ae8f46f8e8ad367bfccae25da6e06f7
Karl O <k@rl.run>
2023-08-24 15:23:07 +0200

    create cron job that keeps distroless digest hash up to date (#1)

--- scripts/common/utils.ts
+++ scripts/common/utils.ts
@@ -2,3 +2,7 @@ export function exitWithMessage(message: string, code: number): never {
     console.log(message)
     process.exit(code)
 }
+
+export function raise(message: string): never {
+    throw new Error(message)
+}
