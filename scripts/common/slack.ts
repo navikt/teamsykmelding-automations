79971faad7f9de9121dcbe8b88ba18d342ac1347
Karl J. Overå <k@rl.run>
2024-08-09 12:39:46 +0200

    feat: truncate to 50 blocks if it exceeds

--- scripts/common/slack.ts
+++ scripts/common/slack.ts
@@ -12,16 +12,35 @@ export async function postBlocks(blocks: any[]) {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            blocks,
+            blocks: limitTo50Blocks(blocks),
         }),
     })
 
     if (result.ok) {
         console.info('Posted to Slack OK')
-        //process.exit(0);
+        process.exit(0)
     } else {
         console.error(`Failed to post to Slack, ${result.status} ${result.statusText}`)
         console.error(await result.text())
-        //process.exit(1);
+        process.exit(1)
     }
+}
+
+function limitTo50Blocks(blocks: any[]) {
+    if (blocks.length <= 50) {
+        return blocks
+    }
+
+    console.warn(`Blocks length ${blocks.length} exceeds 50, truncating`)
+
+    return [
+        ...blocks.slice(0, 49),
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `... and ${blocks.length - 49} more`,
+            },
+        },
+    ]
 }
